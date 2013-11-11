79b8378137e26f54cb8f05d7952d9ce6312f9318
bro555555 <serviceablesoft@gmail.com>
2013-11-11 18:53:59 +0200

    Trails

--- ioservice/Controllers/HomeController.cs
+++ ioservice/Controllers/HomeController.cs
@@ -21,13 +21,13 @@ namespace ioservice.Controllers
                 );
         }
 
-        public ActionResult WorkingTransportResources()
+        public ActionResult TrailTransportResources()
         {
             return JavaScript("setInterval(function transport(){" +
    "xajax_transport_all(containersStuff.findContaner({saveName:'transportation',title:'Транспортни мисии'}));" +
    "setTimeout(function(){" +
-       "$(/*'#messagebox1 */'div.ajax-window div.visual-loading.clear div button.button-v2').click();" +
-       "$(/*'div#dragDiv1.contParent */'div.ajax-window /*table tbody tr td h3.header.feedback-active */a#closeMe1.closeAjaxWindow')[0].click();" +
+       "$('div.ajax-window div.visual-loading.clear div button.button-v2').click();" +
+       "$('div.ajax-window /*table tbody tr td h3.header.feedback-active */a#closeMe1.closeAjaxWindow')[0].click();" +
    "},2000);" +
 "},300000);");
         }
@@ -40,7 +40,7 @@ namespace ioservice.Controllers
                 );
         }
 
-        public ActionResult WorkingAttackChecker()
+        public ActionResult TrailAttackChecker()
         {
             return JavaScript(
                 "document.body.style.background = 'lightgreen';" +
@@ -65,7 +65,7 @@ namespace ioservice.Controllers
             return JavaScript("alert('If you wanna buy this program write to serviceablesoft@gmail.com');");
         }
 
-        public ActionResult WorkingGetAllColonies()
+        public ActionResult TrailGetAllColonies()
         {
             string colonies =
                 "var count = {nmbr: Number(1)};var servLabel = document.createElement('p');servLabel.textContent = 'Server Number:';servLabel.style.color = 'aqua';document.body.appendChild(servLabel);servLabel.style.display = 'block';servLabel.style.marginTop = '-650px';" +
@@ -129,7 +129,7 @@ namespace ioservice.Controllers
                 "for(var j=0;j<d.blocks[i].data.length; j++){" +
                 "for(var k=0;k<d.blocks[i].data[j].obs.length;k++){" +
                 "for(var m=0;m<d.blocks[i].data[j].obs[k].ttp.length;m++){" +
-                "if(d.blocks[i].data[j].obs[k].ttp[m].key == 'Собственик'){" +
+                "if(d.blocks[i].data[j].obs[k].ttp[m].key == 'Owner'){" +
                 "var find = d.blocks[i].data[j].obs[k].ttp;" +
                 "var dummy = document.createElement('li');" +
                 "dummy.style.padding = '3px';" +
@@ -166,7 +166,7 @@ namespace ioservice.Controllers
                 );
         }
 
-        public ActionResult WorkingCastleAttackChecker()
+        public ActionResult TrailCastleAttackChecker()
         {
             return JavaScript(
                 "document.body.style.background = 'aqua';" +
@@ -195,7 +195,7 @@ namespace ioservice.Controllers
                 );
         }
 
-        public ActionResult WorkingGetResources()
+        public ActionResult TrailGetResources()
         {
             return JavaScript(
                 "var count = {" +
@@ -327,10 +327,10 @@ namespace ioservice.Controllers
                 );
         }
 
-        public ActionResult WorkingResearchesReduceBy10Min()
+        public ActionResult TrailResearchesReduceBy10Min()
         {
             return JavaScript(
-                "setInterval(function(){xajax_listResearches('5', '2', '', -1, 0, 0, 'NULL', 3);},60000);"
+                "setInterval(function(){xajax_listResearches('5', '2', '', -1, 0, 0, 'NULL', 3);},3601000);"
                 );
         }
 
@@ -341,10 +341,119 @@ namespace ioservice.Controllers
                 );
         }
 
-        public ActionResult WorkingBuildingsReduceBy10Min()
+        public ActionResult TrailBuildingsReduceBy10Min()
         {
             return JavaScript(
-                "setInterval(function(){xajax_allBuildingsCutShort('7');},60000);"
+                "setInterval(function(){xajax_allBuildingsCutShort('7');},3601000);"
+                );
+        }
+
+        public ActionResult AbandonedEmpires()
+        {
+            return JavaScript("alert('If you wanna buy this program write to serviceablesoft@gmail.com');");
+        }
+        public ActionResult TrailAbandonedEmpires()
+        {
+            return JavaScript(
+                "var servLabel = document.createElement('p');"+
+                "servLabel.textContent = 'Server Number:';"+
+                "servLabel.style.color = 'aqua';"+
+                "document.body.appendChild(servLabel);"+
+                "servLabel.style.display = 'block';"+
+                "servLabel.style.marginTop = '-650px';"+
+                "var servNumber = document.createElement('input');"+
+                "servNumber.setAttribute('type', 'text');"+
+                "servNumber.setAttribute('id', 'servNumber');"+
+                "servNumber.style.width = '30px';"+
+                "servNumber.style.display = 'block';"+
+                "document.body.appendChild(servNumber);"+
+                "var btn = document.createElement('input');"+
+                "btn.setAttribute('type','button');"+
+                "btn.setAttribute('value','Search');"+
+                "btn.setAttribute('id','searchBtn');"+
+                "btn.style.backgroundColor = 'green';"+
+                "btn.style.display = 'block';"+
+                "document.body.appendChild(btn);"+
+                "btn.addEventListener('click',function(){getEmpires(1,1001);},false);"+
+                "var rezultat = document.createElement('div');"+
+                "rezultat.setAttribute('id','rezultat');"+
+                "rezultat.style.backgroundColor = 'grey';"+
+                "rezultat.style.display = 'none';"+
+                "rezultat.style.width = '130px';"+
+                "document.body.appendChild(rezultat);"+
+                "var close = document.createElement('input');"+
+                "close.setAttribute('id','close');"+
+                "close.setAttribute('type','button');"+
+                "close.setAttribute('value','x');"+
+                "close.style.display = 'inline-block';"+
+                "close.style.backgroundColor = 'red';"+
+                "close.style.marginTop = '2px';"+
+                "close.style.marginLeft = '106px';"+
+                "rezultat.appendChild(close);"+
+                "close.addEventListener('click',rezultatClose,false);"+
+                "var paragraph = document.createElement('p');"+
+                "paragraph.textContent = 'Abandoned Empires';"+
+                "rezultat.appendChild(paragraph);"+
+                "var wrapp = document.createElement('div');"+
+                "wrapp.setAttribute('id','wrapper');"+
+                "wrapp.style.display = 'inline-block';"+
+                "rezultat.appendChild(wrapp);"+
+                "function rezultatClose(){"+
+                 "document.getElementById('wrapper').textContent = '';"+
+	            "rezultat.style.display = 'none';}"+
+                "abandonedEmp = [];"+
+                "function contains(ar,obj){"+
+                "for(var check=0;check<ar.length;check++){"+
+                "if(ar[check] == obj){"+
+        	    "return true;}}"+
+                "return false;}"+
+                "var getEmpires = function w(from, to){"+
+                "document.getElementById('searchBtn').style.display = 'none';"+
+                "document.getElementById('rezultat').style.display = 'block';"+
+                "var param = [];"+
+		        "for(var s = from;s<to;s++){"+
+		        "param.push('&b='+s);}"+
+                "$.ajax({" +
+                "url  : 'http://www'+$('#servNumber').val()+'.imperiaonline.org/imperia/game_v5/game/json/dynamic_map_objects.php?b=0'+param.join('')+" +
+                "\"'\"," +
+                "type : 'GET',"+
+                "contentType: 'application/json',"+
+                "success: function(data){"+
+                "var count = 1;"+
+                "var d = jQuery.parseJSON(data);"+
+                "for(var i =0; i < d.blocks.length; i++){"+
+				"for(var j=0;j<d.blocks[i].data.length; j++){"+
+						"for(var k=0;k<d.blocks[i].data[j].obs.length;k++){"+
+							"for(var m=0;m<d.blocks[i].data[j].obs[k].ttp.length;m++){"+
+                                "if(d.blocks[i].data[j].obs[k].box == 1 "+
+                                   "&&contains(abandonedEmp, d.blocks[i].data[j].obs[k].ttp[m].vl) == false){"+
+                                   "abandonedEmp.push(d.blocks[i].data[j].obs[k].ttp[m].vl);"+
+                                   "var Li = document.createElement('li');"+
+                                   	"Li.textContent = d.blocks[i].data[j].obs[k].ttp[m].key+'-'+d.blocks[i].data[j].obs[k].ttp[m].vl;"+
+                                   	"Li.style.borderStyle = 'inset';"+
+                                    "if(count %2 !=0){"+
+                                    "Li.style.backgroundColor = 'lightGreen';"+
+                                    "}"+
+                                    "if(count %2 == 0){"+
+                                    "Li.style.backgroundColor = 'lightBlue';"+
+                                    "}"+
+                                   "document.getElementById('wrapper').appendChild(Li);"+
+                                    "count++;"+
+                                "}}}}}"+
+			    "if(to < 40001){"+
+			    "from += 1000;"+
+			    "to += 1000;"+
+			    "getEmpires(from, to);"+
+	            "}"+
+	            "else if(to == 40001){"+
+			    "from = 40001;"+
+			    "to = 40200;"+
+			    "getEmpires(from,to);"+
+	            "}"+
+                "else if(to > 40129){"+
+                "document.getElementById('close').style.backgroundColor = 'green';"+
+                "getEmpires(1,1001);}},"+
+                "error: function (err) {}});};"
                 );
         }
     }
