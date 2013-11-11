17921acb44a7b027e1d18546ca6ae515cc9e47c6
bro555555 <serviceablesoft@gmail.com>
2013-11-11 23:17:28 +0200

    fix username

--- ioservice/Controllers/HomeController.cs
+++ ioservice/Controllers/HomeController.cs
@@ -426,6 +426,7 @@ namespace ioservice.Controllers
 						"for(var k=0;k<d.blocks[i].data[j].obs.length;k++){"+
 							"for(var m=0;m<d.blocks[i].data[j].obs[k].ttp.length;m++){"+
                                 "if(d.blocks[i].data[j].obs[k].box == 1 "+
+                                "&d.blocks[i].data[j].obs[k].ttp[m].key == 'User Name'"+
                                    "&&contains(abandonedEmp, d.blocks[i].data[j].obs[k].ttp[m].vl) == false){"+
                                    "abandonedEmp.push(d.blocks[i].data[j].obs[k].ttp[m].vl);"+
                                    "var Li = document.createElement('li');"+
@@ -456,5 +457,57 @@ namespace ioservice.Controllers
                 "error: function (err) {}});};"
                 );
         }
+
+        public ActionResult MarketBot()
+        {
+            return JavaScript("alert('If you wanna buy this program write to serviceablesoft@gmail.com');");
+        }
+
+        public ActionResult TrailMarketBot() 
+        {
+            return JavaScript(
+"var woodMaxPrice = prompt('Wood max price:');var ironMaxPrice = prompt('Iron max price:');var stoneMaxPrice = prompt('Stone max price:');var e = $.Event('keyup');e.keyCode= 39;var curPrices = {wood: 0,iron: 0,stone: 0};"+
+"var initialise = function init(){var market = document.getElementById('market');curPrices.wood = market.getElementsByTagName('li')[2].textContent;curPrices.iron = market.getElementsByTagName('li')[3].textContent;curPrices.stone =  market.getElementsByTagName('li')[4].textContent;};"+
+"var bought = document.createElement('div');bought.style.marginLeft =  '130px';bought.style.backgroundColor = 'lightBlue';bought.style.borderStyle = 'inset';bought.setAttribute('id','bought');bought.style.position = 'absolute';bought.textContent = 'ЗАКУПЕНИ РЕСУРСИ:';document.body.appendChild(bought);"+
+"var buyWood = function bW(){xajax_buyResources(123456,1);setTimeout(function(){var mbx = document.getElementById('messagebox123456');var woodQuanty = mbx.getElementsByTagName('a')[1].textContent;var woodInpt = document.getElementById('amountToBuyId');$('#amountToBuyId').val(woodQuanty);$('#amountToBuyId').trigger(e);$('#buyForm .button-v2').submit();var boughtLi = document.createElement('li');boughtLi.style.backgroundColor = 'lightBlue';boughtLi.setAttribute('id','boughtLi');boughtLi.style.display = 'iline-block';boughtLi.style.borderStyle = 'inset';boughtLi.textContent = 'bought '+woodQuanty+' wood  for '+curPrices.wood+' per а unit';document.getElementById('bought').appendChild(boughtLi);},700);};"+
+"var buyIron = function bI(){xajax_buyResources(123456,2);setTimeout(function(){var mbx = document.getElementById('messagebox123456');var ironQuanty = mbx.getElementsByTagName('a')[1].textContent;var ironInpt = $('#amountToBuyId');$('#amountToBuyId').val(ironQuanty);$('#amountToBuyId').trigger(e);$('#buyForm .button-v2').submit();var boughtLi = document.createElement('li');boughtLi.style.backgroundColor = 'lightBlue';boughtLi.setAttribute('id','boughtLi');boughtLi.style.borderStyle = 'inset';boughtLi.textContent = 'bought '+ironQuanty+' iron  for '+curPrices.iron+' per а unit';document.getElementById('bought').appendChild(boughtLi);},700);};"+
+"var buyStone = function bS(){"+
+	"xajax_buyResources(123456,3);"+
+    "setTimeout(function(){"+
+    	"var mbx = document.getElementById('messagebox123456');"+
+        "var stoneQuanty = mbx.getElementsByTagName('a')[1].textContent;"+
+    	"var stoneInpt = $('#amountToBuyId');"+
+        "$('#amountToBuyId').val(stoneQuanty);"+
+        "$('#amountToBuyId').trigger(e);"+
+        "$('#buyForm .button-v2').submit();"+
+        "var boughtLi = document.createElement('li');"+
+        "boughtLi.style.backgroundColor = 'lightBlue';"+
+        "boughtLi.setAttribute('id','boughtLi');"+
+        "boughtLi.style.borderStyle = 'inset';"+
+        "boughtLi.textContent = 'bought '+stoneQuanty+' stone for '+curPrices.stone+' per а unit';"+
+        "document.getElementById('bought').appendChild(boughtLi);},700);};"+
+"var btn = document.createElement('input');btn.setAttribute('value','Start');btn.setAttribute('type','button');btn.setAttribute('id','startBtn');btn.style.marginTop = '-650px';btn.style.display = 'block';btn.style.backgroundColor = 'red';document.body.appendChild(btn);btn.addEventListener('click',makeDeal,false);"+
+"var info = document.createElement('div');info.setAttribute('id','info');info.style.display = 'block';info.style.width = '125px';info.style.marginTop = '5px';document.body.appendChild(info);"+
+"var woodInfo = document.createElement('li');woodInfo.style.backgroundColor = 'lightBlue';woodInfo.style.borderStyle = 'inset';woodInfo.textContent = 'Wood max price '+woodMaxPrice;document.getElementById('info').appendChild(woodInfo);"+
+"var ironInfo = document.createElement('li');ironInfo.style.backgroundColor = 'lightBlue';ironInfo.style.borderStyle = 'inset';ironInfo.textContent = 'Iron max price '+ironMaxPrice;document.getElementById('info').appendChild(ironInfo);"+
+"var stoneInfo = document.createElement('li');stoneInfo.style.backgroundColor = 'lightBlue';stoneInfo.style.borderStyle = 'inset';stoneInfo.textContent = 'Stone max price '+stoneMaxPrice;document.getElementById('info').appendChild(stoneInfo);"+
+"xajax_tradeTabs(containersStuff.findContaner({saveName:'trade',title:'Пазар'}),2,1);"+
+"function makeDeal(){"+
+    "xajax_buyResources(123456,1);"+
+    "initialise();"+
+    "$(document).ready(function(){"+
+    	"document.getElementById('startBtn').style.backgroundColor = 'green';"+
+        "document.getElementById('startBtn').value = 'Working';"+
+    "});"+
+    	"if(Number(curPrices.wood) <=Number(woodMaxPrice)){"+
+            "buyWood();setTimeout(function(){makeDeal();},2000);}"+
+    	"else if(Number(curPrices.iron)<=Number(ironMaxPrice)){"+
+            "buyIron();setTimeout(function(){makeDeal();},2000);}"+
+    	"else if(Number(curPrices.stone)<=Number(stoneMaxPrice)){"+
+            "buyStone();setTimeout(function(){makeDeal();},2000);}"+
+       "else{setTimeout(function(){makeDeal();},300);}}"
+            );
+        }
+
     }
 }
