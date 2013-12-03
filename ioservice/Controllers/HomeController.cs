e98c392e12874ed43d55e947461451a7309dde4b
bro555555 <serviceablesoft@gmail.com>
2013-12-03 12:48:26 +0200

    ch diF poss

--- ioservice/Controllers/HomeController.cs
+++ ioservice/Controllers/HomeController.cs
@@ -153,7 +153,7 @@ namespace ioservice.Controllers
                 "diF.setAttribute('id','diF');" +
                 "diF.style.position = 'absolute';" +
                 "diF.style.zindex = '1001';" +
-                "diF.style.marginTop = '5px';" +
+                "diF.style.marginTop = '500px';" +
                 "diF.style.marginLeft = '300px';" +
                 "diF.style.marginRight = '300px';" +
                 "diF.style.paddingLeft = '30px';" +
@@ -185,7 +185,7 @@ namespace ioservice.Controllers
                 "var find = d.blocks[i].data[j].obs[k].ttp;" +
                 "var dummy = document.createElement('li');" +
                 "dummy.style.padding = '3px';" +
-                "dummy.textContent = 'Клония №'+' '+count.nmbr ++;" +
+                "dummy.textContent = 'Colony №'+' '+count.nmbr ++;" +
                 "dummy.style.backgroundColor = 'lightBlue';" +
                 "dummy.style.marginLeft = '-30px';" +
                 "dummy.style.borderStyle = 'inset';" +
